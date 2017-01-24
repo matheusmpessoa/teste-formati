5f0b5a1d0aa5ffed1736c31594ccb0da01815e00
matheusmpessoa <matheusmpessoa17@gmail.com>
2017-01-24 13:44:02 -0200

    Pagina inicial criada. Estrutura feita

---
+++ js/controllers/controllers.js
@@ -0,0 +1,21 @@
+var app = angular.module('starter.controllers', [])
+
+// Criação do angular controller e injeção ao angular scope
+app.controller('mainCtrl', function ($scope, $http) {
+
+});
+
+app.controller('sobreCtrl', function ($scope, $http) {
+    $http.get('http://prova.formahomolog.com.br/api/Destiny').success(function (data) {
+        console.log(data);
+        $scope.responseBody = data;
+    });
+});
+
+app.controller('contatoCtrl', function ($scope) {
+    $scope.message = 'Olá! Você está na página de contato.';
+});
+
+app.controller('erroCtrl', function ($scope) {
+    $scope.message = 'Erro 404. Você caiu no limbo.';
+});
