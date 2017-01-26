ff928fa2ef35224b928464d76e616687ce958fe4
Matheus <matheusmpessoa17@gmail.com>
2017-01-26 17:48:47 -0200

    Alteracao requisicao de http para //

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -3,7 +3,7 @@ var app = angular.module('starter.controllers', [])
 // Criação do angular controller e injeção ao angular scope
 app.controller('mainCtrl', function ($scope, $http) {
     $scope.responseBody = {};
-    $http.get('http://prova.formahomolog.com.br/api/Destiny').success(function (data) {
+    $http.get('//prova.formahomolog.com.br/api/Destiny').success(function (data) {
         console.log(data);
         $scope.responseBody = data;
     });
